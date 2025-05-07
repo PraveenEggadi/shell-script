316c82468e92c2f01d7afd729d14fb16f369da79
PraveenEggadi <praveeneggadi2@gmail.com>
2025-05-07 19:45:34 +0530

    passing arguements in shell script

---
+++ 04-variables.sh
@@ -0,0 +1,10 @@
+#1/bin/bash
+
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1: Hi $PERSON2, Good morning"
+echo "$PERSON2: Hi $PERSON1, Good morning"
+echo "$PERSON1: How are you $PERSON2?"
+echo "$PERSON2: I'm fine, How are you?"
