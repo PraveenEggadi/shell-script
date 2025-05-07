b23561dd97ebfe53fb14136b875b25b1cec6dc8d
PraveenEggadi <praveeneggadi2@gmail.com>
2025-05-07 19:58:24 +0530

    passing arguements in shell script

--- 05-variables.sh
+++ 05-variables.sh
@@ -5,3 +5,10 @@ echo "Enter your username: "
 read USERNAME # the value entered above will be assigned to USERNAME variable
 
 echo "Username is:  ${USERNAME}"
+
+echo "Enter your password: "
+
+read -s PASSWORD # the value which user enters will not be visible in the terminal but its value will be assigned to the variable
+
+echo "PAssword: ${PASSWORD}
+
