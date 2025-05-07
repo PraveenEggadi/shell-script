4706ca23f7eea569ee7cb8012474deea748e8383
Eggadi Praveen <Eggadi.Praveen@Clarivate.com>
2025-05-07 19:32:51 +0530

    variables in shell-script

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,6 +1,10 @@
 #1/bin/bash
 
-echo "Ramesh: Hi Suresh, Good morning"
-echo "Suresh: Hi Ramesh, Good morning"
-echo "Ramesh: How are you?"
-echo "Suresh: I'm fine, How are you?"
+
+PERSON1=Ramesh
+PERSON2=Suresh
+
+echo "$PERSON1: Hi $PERSON2, Good morning"
+echo "$PERSON2: Hi $PERSON1, Good morning"
+echo "$PERSON1: How are you $PERSON2?"
+echo "$PERSON2: I'm fine, How are you?"
