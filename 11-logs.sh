c739e59d0b70847e57538b514299778df928b49c
PraveenEggadi <praveeneggadi2@gmail.com>
2025-05-08 12:42:30 +0530

    passing arguements in shell script

--- 11-logs.sh
+++ 11-logs.sh
@@ -6,23 +6,26 @@ echo "Script-name: $0"
 TIMESTAMP=$(date +%F-%H-%M-%S)
 
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
+R="\e[31m" # Red color
+G="\e[32m" #Green  color
+N="\e[0m" #Normal colour
 
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo "ERROR: $2 has failed"
+        echo "ERROR: $2 has $R FAILED $N"
         exit 1
     else
-        echo "$2 Success!"
+        echo "$2 $G SUCCESS $N!"
     fi
 }
 
 if [ ${ID} != 0 ]
 then 
-    echo "ERROR: Please run with root access"
+    echo "$R ERROR: Please run with root access $N"
     exit 1
 else
-    echo "Root user"
+    echo "$G Root user $N"
 fi
 
 yum install mysql -y &>> $LOGFILE
