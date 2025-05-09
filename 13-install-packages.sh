480cd8668588a7833584ad31fa84af7b6d35fdd6
PraveenEggadi <praveeneggadi2@gmail.com>
2025-05-09 18:16:46 +0530

    passing arguements in shell script

--- 13-install-packages.sh
+++ 13-install-packages.sh
@@ -5,6 +5,20 @@ ID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
+Y="\e[33m"
+TIMESTAMP= $(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "script started executing at $TIMESTAMP" &>> $LOGFILE
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo "ERROR: $2 has $R FAILED"
+        exit 1
+    else
+        echo "$2 $G SUCCESS!"
+    fi
+}
 
 if [ $ID != 0 ]
 then 
@@ -14,4 +28,15 @@ else
     echo -e "$G Root User $N"
 fi
 
-echo "All Arguments passed: $@"
+# echo "All Arguments passed: $@"
+
+for package in $@
+do
+    yum list installed $package &>> $LOGFILE
+    if [ $? != 0 ]
+    then
+        yum install $package -y &>> $LOGFILE
+        VALIDATE $? "Installation of $package"
+    else
+        echo -e "$package is already installed... $Y SKIPPED $N"
+done
