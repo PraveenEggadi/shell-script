a3c3ec058f602010130f11357377ce32b3659394
PraveenEggadi <praveeneggadi2@gmail.com>
2025-05-11 16:56:09 +0530

    roboshop-shell script code

--- 16-disk-usage.sh
+++ 16-disk-usage.sh
@@ -6,8 +6,8 @@ message=""
 
 while IFS= read line
 do
-    usage=$(echo $line | awk '{print $6F}' | cut -d % -f1)
-    partition=$(echo $line | awk '{print $1F}')
+    usage=$($line | awk '{print $6F}' | cut -d % -f1)
+    partition=$( $line | awk '{print $1F}')
     if [ $usage -ge $partition ]
     then
         message+="High Disk usage alert on $partition:  $usage\n"
