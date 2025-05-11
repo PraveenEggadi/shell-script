49496bb7a2b62114293c38ddfcee3fc16682cad6
PraveenEggadi <praveeneggadi2@gmail.com>
2025-05-11 16:05:44 +0530

    roboshop-shell script code

---
+++ 15-ifs.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+file=/etc/passwd
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ ! -f $file ]
+then
+    echo -e "$R file: $file does not exist $N"
+fi
+
+while IFS= ":" read -r f1 username password userid groupid fullname home_dir shell_path
+do
+    echo "username: $username"
+    echo "id: $userid"
+    echo "full name: $fullname"
+
+done < $file
