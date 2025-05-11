07af8d494723d008e6f4638ec2b050e19e53356b
PraveenEggadi <praveeneggadi2@gmail.com>
2025-05-11 14:52:13 +0530

    roboshop-shell script code

---
+++ 14-delete-old-logs.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+SOURCE_DIR="/tmp/shellscript-logs"
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ ! -d $SOURCE_DIR ]
+then
+    echo "$R Source Directory: $SOURCE_DIR does not exist. $N"
+fi
+
+FILES_To_DEL=$(find $SOURCE_DIR -type f -mtime +14 -name "*.log")
+
+while IFS= read -r line
+do
+    echo "Deleting files: $line"
+done <<<$FILES_TO_DEL
